f468faf4c7ead8dba633cbf87f4f633df9781c15
ViperSRT3g <ViperSRT3g@users.noreply.github.com>
2021-08-02 14:46:40 -0400

    File Creation

---
+++ Mod_ExcelAPIs.vb
@@ -0,0 +1,50 @@
+﻿Imports Microsoft.Office.Interop.Excel
+
+Module Mod_ExcelAPIs
+    Public Function GetLastRow(TSheet As Worksheet, ColumnNo As Long) As Long
+        Return TSheet.Cells(TSheet.Rows.CountLarge, ColumnNo).End(-4162).Row
+    End Function
+
+    Public Function GetLastCol(TSheet As Worksheet, RowNo As Long) As Long
+        Return TSheet.Cells(RowNo, TSheet.Columns.Count).End(-4159).Column
+    End Function
+
+    Public Function GetHeader(TSheet As Worksheet, HeaderRow As Long, HeaderStr As String) As Long
+        Dim Header As Range = TSheet.Rows(HeaderRow).Find(HeaderStr, LookAt:=1)
+        If Not Header Is Nothing Then Return Header.Column
+    End Function
+
+    Public Function GetHeaders(TSheet As Worksheet, HeaderRow As Long, Optional CaseSensitive As Boolean = False) As Dictionary(Of String, Long)
+        Dim Output As New Dictionary(Of String, Long)
+        For ColCounter As Long = 1 To GetLastCol(TSheet, HeaderRow)
+            If CaseSensitive Then 'Headers are untouched
+                Output(CStr(TSheet.Cells(HeaderRow, ColCounter).Value)) = ColCounter
+            Else 'Headers are all Uppercase
+                Output(UCase(CStr(TSheet.Cells(HeaderRow, ColCounter).Value))) = ColCounter
+            End If
+        Next ColCounter
+        Return Output
+    End Function
+
+    Public Function Expand(ByRef Target As Range, ByVal Direction As XlDirection) As Range
+        If Not Target Is Nothing Then Return Target.Parent.Range(Target, Target.End(Direction))
+    End Function
+
+    Public Function GetSheet(SheetName As String, TWB As Workbook) As Worksheet
+        If SheetName.Length = 0 Then Exit Function
+        If TWB Is Nothing Then Exit Function
+
+        Dim Output As Worksheet
+        Try
+            Output = TWB.Worksheets(SheetName)
+        Catch ex As Exception
+            Console.WriteLine(ex.ToString)
+        Finally
+            If Output Is Nothing Then
+                Output = TWB.Worksheets.Add(After:=TWB.Worksheets(TWB.Worksheets.Count))
+                Output.Name = SheetName
+            End If
+        End Try
+        Return Output
+    End Function
+End Module
---
+++ Mod_Hashing.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Security.Cryptography
+
+Module Mod_Hashing
+    Public Function SHA512(ByRef Data As String) As String
+        Using SHA_512 As New SHA512Managed
+            SHA_512.Initialize()
+            SHA_512.ComputeHash(GetBytes(Data))
+            Return GetHex(SHA_512.Hash)
+        End Using
+    End Function
+
+    Public Function SHA256(ByVal Password As String) As Byte()
+        Using SHA_256 As New SHA256Managed
+            SHA_256.Initialize()
+            SHA_256.ComputeHash(GetBytes(Password))
+            Return SHA_256.Hash
+        End Using
+    End Function
+
+    Private Function GetHex(ByRef Data() As Byte) As String
+        Return BitConverter.ToString(Data).Replace("-", "")
+    End Function
+
+    Private Function GetBytes(ByRef Data As String) As Byte()
+        Return System.Text.Encoding.Unicode.GetBytes(Data)
+    End Function
+
+    Private Function T2S(ByVal PlainText As String) As Byte()
+        Return System.Text.Encoding.UTF8.GetBytes(PlainText)
+    End Function
+
+    Private Function S2T(ByVal Data() As Byte) As String
+        Return System.Text.Encoding.UTF8.GetChars(Data)
+    End Function
+End Module
